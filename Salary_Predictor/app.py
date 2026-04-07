b8528a4a8533153f5d0c396ac1760214e632c813
Shaik Asif <asif118shaik@gmail.com>
2026-04-07 22:01:32 +0530

    app.py

--- Salary_Predictor/app.py
+++ Salary_Predictor/app.py
@@ -1,7 +1,10 @@
+import os
 import streamlit as st
 import pickle
 
-# Load model
+if not os.path.exists("model.pkl"):
+    import train
+
 model = pickle.load(open("model.pkl", "rb"))
 
 st.set_page_config(page_title="Salary Predictor", layout="centered")
@@ -9,13 +12,11 @@ st.set_page_config(page_title="Salary Predictor", layout="centered")
 st.title("💰 Salary Predictor")
 st.write("Enter your experience to estimate salary")
 
-# Input
 exp = st.slider("Years of Experience", 0, 20, 1)
 
 if st.button("Predict Salary"):
     prediction = model.predict([[exp]])[0]
 
-    # Convert values
     salary_yearly = prediction
     salary_lpa = salary_yearly / 100000
     salary_monthly = salary_yearly / 12
@@ -23,3 +24,5 @@ if st.button("Predict Salary"):
     st.success(f"💰 Estimated Salary: ₹{salary_yearly:,.0f}")
     st.info(f"📊 {salary_lpa:.2f} LPA")
     st.info(f"📅 ₹{salary_monthly:,.0f} per month")
+
+    st.balloons()
